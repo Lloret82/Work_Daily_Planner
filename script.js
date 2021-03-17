a55ea5557cbc59f4b95235245bff818c2b27bf88
lloret82 <adbmanchester@gmail.com>
2021-03-17 18:00:13 +0000

    updated readme

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    // Hours of the day variable
-    let workHours = [
+    
+    var workHours = [
         "9:00",
         "10:00",
         "11:00",
@@ -15,92 +15,92 @@ $(document).ready(function () {
     // displays current day in header
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-    // For Loop with JQuery draws row hours with assigned classes and IDs
+    // For Loop to create row hours with classes and IDs
     for (i = 0; i < workHours.length; i++) {
         $(`<div class="col-2 time-block"></div>`)
             .text(workHours[i])
             .appendTo(".row");
-        $(`<textarea class="col-8 note-input" id="input-${i}"></textarea>`)
-            .attr("placeholder", "Enter note here")
+        $(`<textarea class="col-8 note-input empty hello" id="time-${i}"></textarea>`)
+            .attr("placeholder", "What you have to do at this time?")
             .appendTo(".row");
         $(`<button class="col-2 btn btn-secondary saveBtn" id="hour${i}"></button>`)
             .text("save")
             .appendTo(".row");
     }
      // Moment.js to get the current date and time
-     let now = new Date().getHours();
+     var now = new Date().getHours();
 
-     // Determine if each row is past, present, or future (24 hour time-slots).... Need to create a better way!
+     // check if time is past, present, or future and assign differen classes
      if (now > 9) {
-         $("#input-0").addClass("past");
+         $("#time-0").addClass("past");
      } else if (now >= 9 && now < 10) {
-         $("#input-0").addClass("present");
+         $("#time-0").addClass("present");
      } else if (now < 9) {
-         $("#input-0").addClass("future");
+         $("#time-0").addClass("future");
      }
  
      if (now > 10) {
-         $("#input-1").addClass("past");
+         $("#time-1").addClass("past");
      } else if (now >= 10 && now < 11) {
-         $("#input-1").addClass("present");
+         $("#time-1").addClass("present");
      } else if (now < 10) {
-         $("#input-1").addClass("future");
+         $("#time-1").addClass("future");
      }
  
      if (now > 11) {
-         $("#input-2").addClass("past");
+         $("#time-2").addClass("past");
      } else if (now >= 11 && now < 12) {
-         $("#input-2").addClass("present");
+         $("#time-2").addClass("present");
      } else if (now < 11) {
-         $("#input-2").addClass("future");
+         $("#time-2").addClass("future");
      }
  
      if (now > 12) {
-         $("#input-3").addClass("past");
+         $("#time-3").addClass("past");
      } else if (now >= 12 && now < 13) {
-         $("#input-3").addClass("present");
+         $("#time-3").addClass("present");
      } else if (now < 12) {
-         $("#input-3").addClass("future");
+         $("#time-3").addClass("future");
      }
  
      if (now > 13) {
-         $("#input-4").addClass("past");
+         $("#time-4").addClass("past");
      } else if (now >= 13 && now < 14) {
-         $("#input-4").addClass("present");
+         $("#time-4").addClass("present");
      } else if (now < 13) {
-         $("#input-4").addClass("future");
+         $("#time-4").addClass("future");
      }
  
      if (now > 14) {
-         $("#input-5").addClass("past");
+         $("#time-5").addClass("past");
      } else if (now >= 14 && now < 15) {
-         $("#input-5").addClass("present");
+         $("#time-5").addClass("present");
      } else if (now < 14) {
-         $("#input-5").addClass("future");
+         $("#time-5").addClass("future");
      }
  
      if (now > 15) {
-         $("#input-6").addClass("past");
+         $("#time-6").addClass("past");
      } else if (now >= 15 && now < 16) {
-         $("#input-6").addClass("present");
+         $("#time-6").addClass("present");
      } else if (now < 15) {
-         $("#input-6").addClass("future");
+         $("#time-6").addClass("future");
      }
  
      if (now > 16) {
-         $("#input-7").addClass("past");
+         $("#time-7").addClass("past");
      } else if (now >= 16 && now < 17) {
-         $("#input-7").addClass("present");
+         $("#time-7").addClass("present");
      } else if (now < 16) {
-         $("#input-7").addClass("future");
+         $("#time-7").addClass("future");
      }
  
      if (now > 17) {
-         $("#input-8").addClass("past");
+         $("#time-8").addClass("past");
      } else if (now >= 17 && now < 18) {
-         $("#input-8").addClass("present");
+         $("#time-8").addClass("present");
      } else if (now < 17) {
-         $("#input-8").addClass("future");
+         $("#time-8").addClass("future");
      }
  
     // Reset button
@@ -110,152 +110,119 @@ $(document).ready(function () {
 
     $(".clear").on("click", function () {
         localStorage.clear();       // clear local storage
-        location.reload();          // reload the page after local storage is cleared out to give a fresh start
+        location.reload();          // reload the page after clear
     });
 
    
-    // Input user data, post to local storage, and save in correct time-slot
-    // 9am
+    // Input user to do list, save to local storage
+    
 
-    let row9amInput = document.querySelector("#input-0");
-    let row9amSaveBtn = document.querySelector("#hour0");
+    var row9am = document.querySelector("#time-0");
 
-    // Display previous input
-    row9amInput.value = localStorage.getItem("stored0");
+    row9am.value = localStorage.getItem("#hour0");
 
-    // Save button clicks and stores the new user input data
-    hour0.addEventListener("click", updateOutput1);
+    hour0.addEventListener("click", output1);
 
-    // Save user input to Local Storage
-    function updateOutput1() {
-        localStorage.setItem("stored0", row9amInput.value);
+    function output1() {
+        localStorage.setItem("9:00", row9am.value);
+        document.getElementById("time-0").value = "";
+
     }
 
-    // 10am
 
-    let row10amInput = document.querySelector("#input-1");
-    let row10amSaveBtn = document.querySelector("#hour1");
+    var row10am = document.querySelector("#time-1");
 
-    // Display previous user input
-    row10amInput.value = localStorage.getItem("stored1");
+    row10am.value = localStorage.getItem("hour1");
 
-    // Save button clicks and stores the user input data
-    hour1.addEventListener("click", userDataStore1);
+    hour1.addEventListener("click", output2);
 
-    // Save user input to Local Storage
-    function userDataStore1() {
-        localStorage.setItem("stored1", row10amInput.value);
+    function output2() {
+        localStorage.setItem("10:00", row10am.value);
+        document.getElementById("time-1").value = "";    }
+
+
+    var row11am = document.querySelector("#time-2");
+
+    row11am.value = localStorage.getItem("hour2");
+
+    hour2.addEventListener("click", output3);
+
+    function output3() {
+        localStorage.setItem("11:00", row11am.value);
+        document.getElementById("time-2").value = "";
     }
 
-    // 11am
 
-    let row11amInput = document.querySelector("#input-2");
-    let row11amSaveBtn = document.querySelector("#hour2");
+    var row12pm = document.querySelector("#time-3");
 
-    // Display previous user input
-    row11amInput.value = localStorage.getItem("stored2");
+    row12pm.value = localStorage.getItem("hour3");
 
-    // Save button clicks and stores the user input data
-    hour2.addEventListener("click", userDataStore2);
+    hour3.addEventListener("click", output4);
 
-    // Save user input to Local Storage
-    function userDataStore2() {
-        localStorage.setItem("stored2", row11amInput.value);
+    function output4() {
+        localStorage.setItem("12:00", row12pm.value);
+        document.getElementById("time-3").value = "";
     }
 
-    // 12pm
 
-    let row12pmInput = document.querySelector("#input-3");
-    let row12pmSaveBtn = document.querySelector("#hour3");
+    var row1pm = document.querySelector("#time-4");
 
-    // Display previous user input
-    row12pmInput.value = localStorage.getItem("12pm");
+    row1pm.value = localStorage.getItem("hour4");
 
-    // Save button clicks and stores the user input data
-    row12pmSaveBtn.addEventListener("click", user12pm);
+    hour4.addEventListener("click", output5);
 
-    // Save user input to Local Storage
-    function user12pm() {
-        localStorage.setItem("12pm", row12pmInput.value);
+    function output5() {
+        localStorage.setItem("13:00", row1pm.value);
+        document.getElementById("time-4").value = "";
     }
 
-    // 1pm
+    
 
-    let row1pmInput = document.querySelector("#input-4");
-    let row1pmSaveBtn = document.querySelector("#hour4");
+    var row2pm = document.querySelector("#time-5");
+   
+    row2pm.value = localStorage.getItem("hour5");
 
-    // Display previous user input
-    row1pmInput.value = localStorage.getItem("stored4");
+    hour5.addEventListener("click", output6);
 
-    // Save button clicks and stores the user input data
-    hour4.addEventListener("click", updateOutput4);
-
-    // Save user input to Local Storage
-    function updateOutput4() {
-        localStorage.setItem("stored4", row1pmInput.value);
+    function output6() {
+        localStorage.setItem("14:00", row2pm.value);
+        document.querySelector(".note-input").value = "";
     }
 
-    // 2pm
 
-    let row2pmInput = document.querySelector("#input-5");
-    let row2pmSaveBtn = document.querySelector("#hour5");
+    var row3pm = document.querySelector("#time-6");
+   
+    row3pm.value = localStorage.getItem("hour6");
 
-    // Display previous user input
-    row2pmInput.value = localStorage.getItem("stored5");
+    
+    hour6.addEventListener("click", output7);
 
-    // Save button clicks and stores the user input data
-    hour5.addEventListener("click", updateOutput5);
-
-    // Save user input to Local Storage
-    function updateOutput5() {
-        localStorage.setItem("stored5", row2pmInput.value);
+    function output7() {
+        localStorage.setItem("15:00", row3pm.value);
+        document.querySelector(".note-input").value = "";
     }
 
-    // 3pm
 
-    let row3pmInput = document.querySelector("#input-6");
-    let row3pmSaveBtn = document.querySelector("#hour6");
+    var row4pm = document.querySelector("#time-7");
 
-    // Display previous user input
-    row3pmInput.value = localStorage.getItem("stored6");
+    row4pm.value = localStorage.getItem("hour7");
 
-    // Save button clicks and stores the user input data
-    hour6.addEventListener("click", updateOutput6);
+    hour7.addEventListener("click", output8);
 
-    // Save user input to Local Storage
-    function updateOutput6() {
-        localStorage.setItem("stored6", row3pmInput.value);
+    function output8() {
+        localStorage.setItem("16:00", row4pm.value);
+        document.querySelector(".note-input").value = "";
     }
 
-    // 4pm
 
-    let row4pmInput = document.querySelector("#input-7");
-    let row4pmSaveBtn = document.querySelector("#hour7");
+    var row5pm = document.querySelector("#time-8");
 
-    // Display previous user input
-    row4pmInput.value = localStorage.getItem("stored7");
+    row5pm.value = localStorage.getItem("hour8");
 
-    // Save button clicks and stores the user input data
-    hour7.addEventListener("click", updateOutput7);
+    hour8.addEventListener("click", output9);
 
-    // Save user input to Local Storage
-    function updateOutput7() {
-        localStorage.setItem("stored7", row4pmInput.value);
-    }
-
-    // 5pm
-
-    let row5pmInput = document.querySelector("#input-8");
-    let row5pmSaveBtn = document.querySelector("#hour8");
-
-    // Display previous user input
-    row5pmInput.value = localStorage.getItem("stored8");
-
-    // Save button clicks and stores the user input data
-    hour8.addEventListener("click", updateOutput8);
-
-    // Save user input to Local Storage
-    function updateOutput8() {
-        localStorage.setItem("stored8", row5pmInput.value);
+    function output9() {
+        localStorage.setItem("17:00", row5pm.value);
+        document.querySelector(".note-input").value = "";
     }
 });
